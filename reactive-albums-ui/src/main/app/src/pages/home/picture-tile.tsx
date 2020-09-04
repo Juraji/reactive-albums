ca9150bfeb64002497ae501bb2724411563f270b
Juraji <github@juraji.nl>
2020-09-05 01:37:04 +0200

    Display tag orbs and duplicate count in picture tiles

--- reactive-albums-ui/src/main/app/src/pages/home/picture-tile.tsx
+++ reactive-albums-ui/src/main/app/src/pages/home/picture-tile.tsx
@@ -1,10 +1,21 @@
-import React, { FC } from 'react';
-import { Picture } from '@types';
+import React, { FC, useMemo } from 'react';
+import { Picture, Tag } from '@types';
 import { useApiUrl, useFileSize, useHumanDate } from '@hooks';
 import Card from 'react-bootstrap/Card';
 
 import './picture-tile.scss';
 import { Link } from 'react-router-dom';
+import Badge from 'react-bootstrap/Badge';
+import { Conditional } from '@components';
+
+interface TagOrbProps {
+  tag: Tag;
+}
+
+export const TagOrb: FC<TagOrbProps> = ({ tag }) => {
+  const style = useMemo(() => ({ backgroundColor: `#${tag.labelColor}` }), [tag]);
+  return <div className="tag-orb" title={tag.label} style={style} />;
+};
 
 interface PictureTileProps {
   picture: Picture;
@@ -16,8 +27,10 @@ export const PictureTile: FC<PictureTileProps> = ({ picture }) => {
   const fileSizeHuman = useFileSize(picture.fileSize);
 
   return (
-    <Card className="picture-tile mb-4" as={Link} to={`/picture/${picture.id}`}>
-      <Card.Img variant="top" src={imageUrl} />
+    <Card className="picture-tile mb-4">
+      <Link to={`/picture/${picture.id}`}>
+        <Card.Img variant="top" src={imageUrl} />
+      </Link>
       <Card.Body>
         <ul className="list-unstyled">
           <li className="font-weight-bold">{picture.displayName}</li>
@@ -26,6 +39,16 @@ export const PictureTile: FC<PictureTileProps> = ({ picture }) => {
           </li>
           <li className="small">{lastModified}&nbsp;</li>
         </ul>
+        <div className="tag-orbs">
+          {picture.tags.map((tag, index) => (
+            <TagOrb tag={tag} key={index} />
+          ))}
+        </div>
+        <Conditional condition={picture.duplicateCount > 0}>
+          <Badge variant="danger" className="duplicate-count">
+            {picture.duplicateCount}
+          </Badge>
+        </Conditional>
       </Card.Body>
     </Card>
   );
