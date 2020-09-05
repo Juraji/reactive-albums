4b592991dca9afe8f70b3035746d677449aea342
Juraji <github@juraji.nl>
2020-09-05 16:19:16 +0200

    Add revisions to ContentHashUpdatedEvent and ThumbnailLocationUpdatedEvent

--- reactive-albums-api/src/main/kotlin/nl/juraji/reactive/albums/domain/pictures/events/ContentHashUpdatedEvent.kt
+++ reactive-albums-api/src/main/kotlin/nl/juraji/reactive/albums/domain/pictures/events/ContentHashUpdatedEvent.kt
@@ -1,8 +1,10 @@
 package nl.juraji.reactive.albums.domain.pictures.events
 
 import nl.juraji.reactive.albums.domain.pictures.PictureId
+import org.axonframework.serialization.Revision
 import java.util.*
 
+@Revision("1.0")
 data class ContentHashUpdatedEvent(
         override val pictureId: PictureId,
         val contentHash: BitSet,
--- reactive-albums-api/src/main/kotlin/nl/juraji/reactive/albums/domain/pictures/events/ThumbnailLocationUpdatedEvent.kt
+++ reactive-albums-api/src/main/kotlin/nl/juraji/reactive/albums/domain/pictures/events/ThumbnailLocationUpdatedEvent.kt
@@ -2,8 +2,10 @@ package nl.juraji.reactive.albums.domain.pictures.events
 
 import nl.juraji.reactive.albums.domain.pictures.PictureId
 import nl.juraji.reactive.albums.domain.pictures.PictureType
+import org.axonframework.serialization.Revision
 import java.nio.file.Path
 
+@Revision("1.0")
 data class ThumbnailLocationUpdatedEvent(
         override val pictureId: PictureId,
         val thumbnailLocation: Path,
