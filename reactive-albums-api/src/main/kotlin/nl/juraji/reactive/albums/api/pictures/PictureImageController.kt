769883ed58b2b2298b75e654b0670cbb421cf865
Juraji <github@juraji.nl>
2020-09-08 19:45:01 +0200

    Set cache control for thumbnails

--- reactive-albums-api/src/main/kotlin/nl/juraji/reactive/albums/api/pictures/PictureImageController.kt
+++ reactive-albums-api/src/main/kotlin/nl/juraji/reactive/albums/api/pictures/PictureImageController.kt
@@ -6,11 +6,14 @@ import nl.juraji.reactive.albums.query.thumbnails.repositories.ReactiveThumbnail
 import org.springframework.core.io.ByteArrayResource
 import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.Resource
+import org.springframework.http.CacheControl
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.switchIfEmpty
+import java.time.Duration
 
 
 @RestController
@@ -22,10 +25,14 @@ class PictureImageController(
     @GetMapping("/api/pictures/{pictureId}/thumbnail")
     fun getPictureThumbnail(
             @PathVariable("pictureId") pictureId: String,
-    ): Mono<Resource> = thumbnailRepository
+    ): Mono<ResponseEntity<Resource>> = thumbnailRepository
             .findById(pictureId)
             .switchIfEmpty { Mono.error { NoSuchEntityException("Thumbnail", pictureId) } }
-            .map { ByteArrayResource(it.thumbnail) }
+            .map {
+                ResponseEntity.ok()
+                        .cacheControl(thumbnailCacheControl)
+                        .body(ByteArrayResource(it.thumbnail))
+            }
 
     @GetMapping("/api/pictures/{pictureId}/image")
     fun getPictures(
@@ -34,4 +41,9 @@ class PictureImageController(
             .findPictureImageById(pictureId)
             .switchIfEmpty { Mono.error(NoSuchEntityException("Picture", pictureId)) }
             .map { FileSystemResource(it.location) }
+
+    companion object{
+        val thumbnailCacheControl = CacheControl
+                .maxAge(Duration.ofDays(1))
+    }
 }
