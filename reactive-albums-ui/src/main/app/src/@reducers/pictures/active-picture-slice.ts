f0e7e00ccf74937c9748d3c61ba36d2fdd4e611e
Juraji <github@juraji.nl>
2020-09-20 02:18:11 +0200

    deactivate active picture on delete succes

--- reactive-albums-ui/src/main/app/src/@reducers/pictures/active-picture-slice.ts
+++ reactive-albums-ui/src/main/app/src/@reducers/pictures/active-picture-slice.ts
@@ -1,6 +1,6 @@
 import { combineReducers, createEntityAdapter, createReducer, EntityState } from '@reduxjs/toolkit';
 import { DuplicateMatch, Picture } from '@types';
-import { activatePictureById, movePicture } from './picture.thunks';
+import { activatePictureById, deletePicture, movePicture } from './picture.thunks';
 import {
   addActivePictureDuplicateMatch,
   deactivatePicture,
@@ -28,6 +28,7 @@ const activePictureReducer = createReducer<Picture | null>(null, (builder) => {
   builder.addCase(activatePictureById.fulfilled, (_, { payload }) => payload);
   builder.addCase(deactivatePicture, () => null);
   builder.addCase(movePicture.fulfilled, (state, { payload }) => payload);
+  builder.addCase(deletePicture.fulfilled, () => null);
 });
 
 export const activePictureSliceName = 'activePicture';
