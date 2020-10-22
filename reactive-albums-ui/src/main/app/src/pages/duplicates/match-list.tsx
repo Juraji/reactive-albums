b4ea34c493ad106b28dc05244801ecba7f2e0957
Juraji <github@juraji.nl>
2020-10-22 18:37:24 +0200

    Rework duplicate view match list layout

--- reactive-albums-ui/src/main/app/src/pages/duplicates/match-list.tsx
+++ reactive-albums-ui/src/main/app/src/pages/duplicates/match-list.tsx
@@ -1,6 +1,33 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { DuplicateMatch } from '@types';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Table from 'react-bootstrap/Table';
+
+import './match-list.scss';
+
+interface MatchListItemProps {
+  match: DuplicateMatch;
+  selectedMatch: DuplicateMatch | undefined;
+  onMatchSelect: (match: DuplicateMatch) => void;
+}
+
+export const MatchListItem: FC<MatchListItemProps> = ({ match, selectedMatch, onMatchSelect }) => {
+  const rowClassName = useMemo(() => {
+    const classes = ['cursor-pointer'];
+
+    if (match.id === selectedMatch?.id) {
+      classes.push('active');
+    }
+
+    return classes.join(' ');
+  }, [match, selectedMatch]);
+
+  return (
+    <tr className={rowClassName} onClick={() => onMatchSelect(match)}>
+      <td>{match.picture?.displayName}</td>
+      <td className="font-weight-bold">{match.similarity}%</td>
+    </tr>
+  );
+};
 
 interface MatchListProps {
   matches: DuplicateMatch[];
@@ -8,20 +35,14 @@ interface MatchListProps {
   onMatchSelect: (match: DuplicateMatch) => void;
 }
 
-export const MatchList: FC<MatchListProps> = ({ matches, selectedMatch, onMatchSelect }) => (
-  <ListGroup>
-    {matches.map((dm, idx) => (
-      <ListGroup.Item
-        key={idx}
-        action
-        onClick={() => onMatchSelect(dm)}
-        active={dm.id === selectedMatch?.id}
-        className="d-flex flex-row"
-      >
-        <span className="flex-grow-1 text-ellipsis text-center">{dm.picture?.displayName}</span>
-        <span className="mx-2 font-weight-bold">&lt;{dm.similarity}%&gt;</span>
-        <span className="flex-grow-1 text-ellipsis text-center">{dm.target?.displayName}</span>
-      </ListGroup.Item>
-    ))}
-  </ListGroup>
-);
+export const MatchList: FC<MatchListProps> = ({ matches, ...rest }) => {
+  return (
+    <Table hover responsive size="sm" className="match-list-table">
+      <tbody>
+        {matches.map((dm, idx) => (
+          <MatchListItem key={idx} match={dm} {...rest} />
+        ))}
+      </tbody>
+    </Table>
+  );
+};
